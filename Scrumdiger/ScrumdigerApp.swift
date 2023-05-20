6a91f5b99e4a7a902b5742e65f8d6c5b53e4a719
Gonzalo Vazquez <gonzalovazquez010@gmail.com>
2023-05-19 21:02:00 -0400

    Initial Commit

---
+++ Scrumdiger/ScrumdigerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdigerApp.swift
+//  Scrumdiger
+//
+//  Created by Gonzalo Vazquez on 2023-05-19.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdigerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
